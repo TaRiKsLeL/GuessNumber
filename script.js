a13428a2bbd8e4b2287dcf8f5233319edef16800
Taras <tariksyurts@gmail.com>
2020-05-02 01:28:25 +0300

    design

--- script.js
+++ script.js
@@ -1,89 +1,93 @@
-$(document).ready(function(){ // Якщо документ загрузився
+$(document).ready(function () {
+  // Якщо документ загрузився
 
-var users=[];
-var currentUserName='';    
+  var users = [];
+  var currentUserName = "";
 
-var onReload = false;
+  var onReload = false;
 
-var rand_num = Math.floor(Math.random() * 100); // загадуємо число
-console.log(rand_num); // :) робимо сибі підказку
+  var rand_num = Math.floor(Math.random() * 100); // загадуємо число
+  console.log(rand_num); // :) робимо сибі підказку
 
-function createNewUser(userName, attempts){
+  function createNewUser(userName, attempts) {
     users.push({
-        name: userName,
-        attempts: attempts,
+      name: userName,
+      attempts: attempts,
     });
-}
+  }
 
-function createUsersTable(){
-    
-    let headerTable = `<tr><th>Ім'я</th><th>Спроби</th></tr>`
-    let innerTable =  users.reduce(function(res,curr){
-        return res += `<tr> 
+  function createUsersTable() {
+    let headerTable = `<tr><th>Ім'я</th><th>Спроби</th></tr>`;
+    let innerTable = users.reduce(function (res, curr) {
+      return (res += `<tr> 
                 <td> ${curr.name}</td>
                 <td> ${curr.attempts}</td>
-                </tr>`;
-    },'');
-    return "<table>"+headerTable+innerTable+"</table>";
-}
+                </tr>`);
+    }, "");
+    return "<table class='users-table'>" + headerTable + innerTable + "</table>";
+  }
 
-$("#input_num").click(function() { //після нажання кнопки
-    if(onReload){
-        $(this).html('Випробувати вдачу');
-        rand_num = Math.floor(Math.random() * 100);
-        console.log(rand_num);
-        $("#user_name").prop('disabled', false);
-        $("#you_num").prop('disabled', false);
-        onReload=false;
-        return;
+  $("#input_num").click(function () {
+    //після нажання кнопки
+    if (onReload) {
+      $(this).html("Випробувати вдачу");
+      rand_num = Math.floor(Math.random() * 100);
+      console.log(rand_num);
+      $("#user_name").prop("disabled", false);
+      $("#you_num").prop("disabled", false);
+      onReload = false;
+      return;
     }
 
-    let inputName = $('#user_name').val();
-    var num = $('#you_num').val(); // берем своє введене число
+    let inputName = $("#user_name").val();
+    var num = $("#you_num").val(); // берем своє введене число
 
-    if(currentUserName!== inputName && users.some(user=>user.name===inputName)){ // Якщо ввели користувача, який раніше був введений, очистити дані про нього 
-        users.find(user=>user.name===inputName).attempts =0;
+    if (currentUserName !== inputName && users.some((user) => user.name === inputName)) {
+      // Якщо ввели користувача, який раніше був введений, очистити дані про нього
+      users.find((user) => user.name === inputName).attempts = 0;
     }
-    
+
     currentUserName = inputName;
-    
-    if(users.some(user=>user.name===currentUserName)){                  //Якщо міститься такий користувач в списку, якщо ні - створити
-        users.find(user=>user.name===currentUserName).attempts +=1;
-    }else{
-        console.log("adding user");
-        createNewUser(currentUserName,1);
-    }
 
-    let hint = '';
-    let subHint='';
-
-    if (num == rand_num) { // і перевіряємо чи вгадали
-        hint += `success <br> Game over` ; // відповідно вітаємо з перемогою
-        win();
+    if (users.some((user) => user.name === currentUserName)) {
+      //Якщо міститься такий користувач в списку, якщо ні - створити
+      users.find((user) => user.name === currentUserName).attempts += 1;
     } else {
-        hint += `неправильно <br>` ; // або кажемо що не вгадав
-        $('#rez .hint').css('color','red');
-      if(num>rand_num){ // і хочу щоб була підказка
-            subHint += `дане число більше загаданого`;
-        }
-        if(num<rand_num){
-            subHint += `дане число менше загаданого`;
-        }
+      console.log("adding user");
+      createNewUser(currentUserName, 1);
     }
-    $('#rez .hint').html(hint);
-    $('#rez .sub-hint').html(subHint);
-});
 
-function win(){
-        onReload=true;
-        $('#input_num').html("Попробувати ще раз");
-        users.sort((prevUser,nextUser)=>prevUser.attempts-nextUser.attempts);
-        $("#user_name").prop('disabled', true);
-        $("#you_num").prop('disabled', true);
-        $('.rating').html(createUsersTable());
-        $('#rez .hint').css('color','green');
-        $('#user_name').val('');
-        $('#you_num').val('');
-}
+    let hint = "";
+    let subHint = "";
 
+    if (num == rand_num) {
+      // і перевіряємо чи вгадали
+      hint += `success <br> Game over`; // відповідно вітаємо з перемогою
+      win();
+    } else {
+      hint += `неправильно <br>`; // або кажемо що не вгадав
+      $("#rez .hint").css("color", "#c03d5c");
+      if (num > rand_num) {
+        // і хочу щоб була підказка
+        subHint += `дане число більше загаданого`;
+      }
+      if (num < rand_num) {
+        subHint += `дане число менше загаданого`;
+      }
+    }
+    $("#rez .hint").html(hint);
+    $("#rez .sub-hint").html(subHint);
+  });
+
+  function win() {
+    onReload = true;
+    $("#input_num").html("Попробувати ще раз");
+    users.sort((prevUser, nextUser) => prevUser.attempts - nextUser.attempts);
+    $("#user_name").prop("disabled", true);
+    $("#you_num").prop("disabled", true);
+    $(".rating-text").html(`<p>Рейтинг</p>` + createUsersTable());
+    $("#rez .hint").css("color", "#ffcd7a");
+    $("#user_name").val("");
+    $("#you_num").val("");
+  }
 });
